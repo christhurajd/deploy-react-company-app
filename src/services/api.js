985c649567000123fa461bb112ba74238eb650fb
christhurajd <christhurajshc@gmail.com>
2026-02-04 15:04:17 +0530

    changes in basurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,11 @@
-const API_BASE_URL = "https://apcs-api-hyceezatfxfhhta8.canadacentral-01.azurewebsites.net";
+import axios from "axios";
 
-export default API_BASE_URL;
+const API = axios.get({
+  baseURL: "https://apcs-api-hyceezatfxfhhta8.canadacentral-01.azurewebsites.net", // use REACT_APP_API_URL if CRA
+});
+
+export default API;
+
+// const API_BASE_URL = "https://apcs-api-hyceezatfxfhhta8.canadacentral-01.azurewebsites.net";
+
+// export default API_BASE_URL;
