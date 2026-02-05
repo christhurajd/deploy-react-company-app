62ea1acaae683aa5c6dc777b7fc6e9f6ba9404e6
christhurajd <christhurajshc@gmail.com>
2026-02-05 14:56:35 +0530

    implemeted to create blogs dynamically using create rest api

---
+++ src/services/apiClient.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+REACT_APP_API_URL="https://apcs-api-hyceezatfxfhhta8.canadacentral-01.azurewebsites.net/api"
+
+const apiClient = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    "Content-Type": "multipart/form-data"
+  }
+});
+
+export default apiClient;
